fae7f5ec693babfd49497cb258a695097690240b
Liam Moore <liam.moore.dev@gmail.com>
2020-07-17 16:21:13 +0000

    finds duplicates

--- names/names.py
+++ names/names.py
@@ -1,3 +1,49 @@
+class BSTNode:
+    def __init__(self, value = None):
+        self.value = value
+        self.count = 1
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if self.value == None:
+            self.value = value
+            return
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        elif value == self.value:
+            self.count += 1
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+        else:
+            if self.right:
+                return self.right.contains(target)
+        return False
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        if self.left:
+            self.left.for_each(fn)
+        fn(self)
+        if self.right:
+            self.right.for_each(fn)
+
 import time
 
 start_time = time.time()
@@ -13,10 +59,20 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+bst = BSTNode()
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+for name_2 in names_2:
+    bst.insert(name_2)
+
+def has_duplicates(node):
+    if node.count > 1:
+        duplicates.append(node.value)
+
+
+bst.for_each(has_duplicates)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
